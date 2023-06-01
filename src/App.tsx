b2f51eaf5c4d9204364869c77a5d60d8dea649fc
Eric AUDRAN <ericaudran@MacBook-Pro-de-Eric.local>
2023-06-01 20:29:19 +0200

    Clean repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import "./App.css";
 import Calendar from "./components/Calendar";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Calendar />
     </div>
   );
